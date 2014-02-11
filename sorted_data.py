03978f77515fb6ec786a7019224344018e961c36
Hackbright Student <info@hackbrightacademy.com>
2014-02-10 17:44:23 -0800

    modified to use sorted()

--- sorted_data.py
+++ sorted_data.py
@@ -14,8 +14,11 @@ for line in txt:
     if len(split_txt) == 2:
         restaurant_scores[split_txt[0]]=split_txt[1]
 
-thekeys = restaurant_scores.keys()
-thekeys.sort()
+# thekeys = restaurant_scores.keys()
+# thekeys.sort()
 
-for k in thekeys:
-    print "Restaurant %r is rated %d" % (k, int(restaurant_scores[k]))
+# for k in thekeys:
+#     print "Restaurant %r is rated %d" % (k, int(restaurant_scores[k]))
+
+for r in sorted(restaurant_scores, key=restaurant_scores.get, reverse=True):
+    print "Restaurant %r is rated %d" % (r, int(restaurant_scores[r]))
